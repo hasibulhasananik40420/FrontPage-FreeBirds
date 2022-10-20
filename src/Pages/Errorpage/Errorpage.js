9e15b9b4898c582ea6e5ced304370eaf982a5b01
teamdeciders <team.thedeciders@gmail.com>
2022-10-20 22:29:33 +0600

    first commit

---
+++ src/Pages/Errorpage/Errorpage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Errorpage = () => {
+    return (
+        <div>
+            <img src="https://miro.medium.com/max/1400/1*zE2qnVTJehut7B8P2aMn3A.gif" alt="" />
+        </div>
+    );
+};
+
+export default Errorpage;
