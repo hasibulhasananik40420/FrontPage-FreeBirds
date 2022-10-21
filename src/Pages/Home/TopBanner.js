bcbef0a22f7f9883b4daf41d707a8ab3d2e2aeb2
teamdeciders <team.thedeciders@gmail.com>
2022-10-21 21:58:39 +0600

    Fixed issues

--- src/Pages/Home/TopBanner.js
+++ src/Pages/Home/TopBanner.js
@@ -13,11 +13,11 @@ const TopBanner = () => {
                     <h3 className='text-lg mb-4  font-bold font-poppins'>HTML, CSS ,BOOTSTRAP,TAILWIND CSS , JAVASCRIPT , FIGMA. </h3>
                     <h3 className='text-lg'>HTML, CSS, ওয়েবসাইট লেআউট, রেসপন্সিভ ও ইন্ট্যারাক্টিভ ডিজাইনের মাধ্যমে ওয়েব ডিজাইনের জন্য প্রয়োজনীয় স্কিলের শেখার পাশাপাশি ওয়েব ডিজাইনার হিসেবে ক্যারিয়ার শুরু করার পরিপূর্ণ গাইডলাইন রয়েছে এই কোর্সে!
                     </h3>
-                    <button className='my-4 py-2 px-4 rounded-full hover:bg-black hover:text-white cardShadow text-xl duration-500'>বিস্তারিত পড়ুন</button>
+                    <button className='bg-white my-4 py-2 px-4 rounded-full hover:bg-black hover:text-white cardShadow text-xl duration-500'>বিস্তারিত পড়ুন</button>
                 </div>
                 {/* Course Card */}
                 <div className=" p-4 w-full">
-                    <div className='lg:w-3/4 cardShadow rounded-md p-3 mx-auto'>
+                    <div className='lg:w-3/4 cardShadow rounded-md p-3 mx-auto bg-white'>
                         <img className='w-full rounded-md mb-3' src="https://i.ibb.co/P9RLm07/Complete-Web-Design-Course.jpg" alt="" />
                         <button className='mt-3 py-3 w-full text-lg text-white bg-indigo-600 rounded-md'>রেজিস্ট্রেশন করুন</button>
                         <div className="lg:flex  mt-3 ">
